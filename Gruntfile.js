2567dd32f5c89e500ab186fc9485b7583cb28863
Igor Hlina <srigi@srigi.sk>
2013-01-03 18:59:12 +0100

    Unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,12 +17,32 @@ module.exports = function(grunt) {
       },
 
       gruntfile: 'Gruntfile.js',
-      tasks: 'tasks/*.js'
+      tasks: 'tasks/*.js',
+      tests: '<%= nodeunit.tasks %>'
     },
 
     // Before generating any new files, remove any previously-created files.
     clean: {
       test: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    targethtml: {
+      dev: {
+        files: {
+          'tmp/dev.html': 'test/fixtures/index.html'
+        }
+      },
+      dist: {
+        files: {
+          'tmp/dist.html': 'test/fixtures/index.html'
+        }
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tasks: ['test/*_test.js']
     }
   });
 
@@ -32,7 +52,13 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'targethtml', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'test']);
 };
