620c3cc722798e031c96db8567f94e79c91c36eb
Matthieu Achard <matthieu@jukeo.net>
2013-09-15 22:54:31 +0200

    conditional expressions support

--- test/targethtml_test.js
+++ test/targethtml_test.js
@@ -6,7 +6,7 @@ exports.targethtml = function(test){
 
   var expected, result;
 
-  test.expect(4);
+  test.expect(6);
 
   expected = grunt.file.read('test/expected/dev.html');
   result = grunt.file.read('tmp/dev.html');
@@ -21,6 +21,14 @@ exports.targethtml = function(test){
   expected = grunt.file.read('test/expected/release.html');
   result = grunt.file.read('tmp/release.html');
   test.equal(result, expected, 'should process :release target');
+
+
+  expected = grunt.file.read('test/expected/foo.html');
+  result = grunt.file.read('tmp/foo.html');
+  test.equal(result, expected, 'should process :foo target');
+  expected = grunt.file.read('test/expected/bar.html');
+  result = grunt.file.read('tmp/bar.html');
+  test.equal(result, expected, 'should process :bar target');
 
   test.done();
 
